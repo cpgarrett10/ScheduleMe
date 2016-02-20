f2f84bf8f5b3866e6c7eb732e9d1741ab05386ad
Carson Garrett <cpgarrett10@gmail.com>
2016-02-20 13:36:30 -0700

    My first commit

---
+++ ScheduleMe/ScheduleMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScheduleMe
+//
+//  Created by Carson Garrett on 2/20/16.
+//  Copyright © 2016 Carson Garrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

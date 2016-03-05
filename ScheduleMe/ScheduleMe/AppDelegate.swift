fe6bdb8b58000fda94d14017fb576d9eba21cbd4
Kyle Tucker <kyle.tucker@domo.com>
2016-03-05 12:17:16 -0700

    garbage commit

--- ScheduleMe/ScheduleMe/AppDelegate.swift
+++ ScheduleMe/ScheduleMe/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 import CoreData
+//import FBSDKCoreKit
+//import FBSDKLoginKit
+//import FBSDKShareKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,8 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+        return true //FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+    
+   // func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+   //     return FBSDKApplicationDelegate.sharedInstance().application(application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+   // }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -36,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- ScheduleMe/ScheduleMe/ViewController.swift
+++ ScheduleMe/ScheduleMe/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
+    var login:Int = 1
+    @IBOutlet var accountLabel: UILabel!
+    
     @IBOutlet var weatherCondition: UILabel!
     var ref = Firebase(url: "https://schedulemecapstone.firebaseio.com/")
 
+    @IBAction func SignUp(sender: AnyObject) {
+        login = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,15 @@ class ViewController: UIViewController {
         ref.observeEventType(.Value, withBlock: {
             snapshot in
             self.weatherCondition.text = snapshot.value as? String
+            
+            
+            
+            if self.login == 1 {
+                
+                self.accountLabel.text = "Already have an account?"
+                
+            }
+            
             
         })
         

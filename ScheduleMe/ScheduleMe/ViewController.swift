040482943eb9fa2fa59bc5ed89135d129832c2a3
Kyle Tucker <kyle.tucker@domo.com>
2016-03-05 13:24:47 -0700

    login and signup work

--- ScheduleMe/ScheduleMe/ViewController.swift
+++ ScheduleMe/ScheduleMe/ViewController.swift
@@ -10,34 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var login = false
+    var login = true
     
     @IBOutlet var accountLabel: UILabel!
     @IBOutlet var loginSignUp: UIButton!
     @IBOutlet var switchLoginSceen: UIButton!
-    
+    @IBOutlet var UsernameTxt: UITextField!
+    @IBOutlet var PasswordTxt: UITextField!
     @IBOutlet var weatherCondition: UILabel!
+    
     var ref = Firebase(url: "https://schedulemecapstone.firebaseio.com/")
 
     @IBAction func SignUp(sender: AnyObject) {
         
-        
-        if self.login == false {
+        if self.login == true {
             
-            login = true
+            login = false
             
             self.accountLabel.text = "Already have an account?"
             self.switchLoginSceen.setTitle("Login", forState: .Normal)
             self.loginSignUp.setTitle("Sign Up", forState: .Normal)
+            self.UsernameTxt.text = ""
+            self.PasswordTxt.text = ""
             
         } else {
             
-            login = false
+            login = true
             
             self.accountLabel.text = "Don't have an account?"
             self.switchLoginSceen.setTitle("Sign Up", forState: .Normal)
             self.loginSignUp.setTitle("Login", forState: .Normal)
+            self.UsernameTxt.text = ""
+            self.PasswordTxt.text = ""
         
+        }
+    
+    }
+    
+    @IBAction func loginSignUp(sender: AnyObject) {
+    
+        if self.login == true {
+            //Login logic
+            ref.authUser(UsernameTxt.text, password: PasswordTxt.text,
+                withCompletionBlock: { error, authData in
+                    if error != nil {
+                        // There was an error logging in to this account
+                        
+                        print("You have failed to loggin" + error.debugDescription)
+                    } else {
+                        // We are now logged in
+                        print("You have successfully logged in")
+                        
+                        //send them to home screen
+                    }
+            })
+            
+            
+        } else {
+            
+            ref.createUser(UsernameTxt.text, password: PasswordTxt.text,
+                withValueCompletionBlock: { error, result in
+                    if error != nil {
+                        // There was an error creating the account
+                        print("This account already exists " + error.debugDescription)
+                        
+                    } else {
+                        let uid = result["uid"] as? String
+                        print("Successfully created user account with uid: \(uid)")
+                        
+                        //send them to first time experience
+                        
+                    }
+            })
+            //sign up logic
+            
         }
     
     }
@@ -49,10 +95,6 @@ class ViewController: UIViewController {
         ref.observeEventType(.Value, withBlock: {
             snapshot in
             self.weatherCondition.text = snapshot.value as? String
-            
-            
-            
-            
             
             
         })

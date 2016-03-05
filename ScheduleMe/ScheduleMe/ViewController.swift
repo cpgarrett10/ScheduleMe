54a8ca1d01d0993d41dba2d11f1aab8645ba3ae2
Carson Garrett <cpgarrett10@gmail.com>
2016-03-05 10:31:36 -0700

    Fix Error

--- ScheduleMe/ScheduleMe/ViewController.swift
+++ ScheduleMe/ScheduleMe/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {

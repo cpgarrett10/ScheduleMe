067b3917c336c942b57a439906453c498a5e98ac
Kyle Tucker <kyle.tucker@domo.com>
2016-03-12 12:56:18 -0700

    data input

--- ScheduleMe/ScheduleMe/UserProfileViewController.swift
+++ ScheduleMe/ScheduleMe/UserProfileViewController.swift
@@ -11,10 +11,63 @@ import UIKit
 class UserProfileViewController : UIViewController {
     
     var ref = Firebase(url: "https://schedulemecapstone.firebaseio.com/")
+    var kyleisawesome = ["FirstName":"Kyle", "LastName":"Tucker", "Email":"cpgarrett10@gmail.com"]
+    
+
+    
+    
+    @IBOutlet var FirstNameTxt: UITextField!
+    @IBOutlet var LastNameTxt: UITextField!
+    @IBOutlet var EmailTxt: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        /*
+        ref.observeEventType(.Value, withBlock: {
+            snapshot in
+            self.weatherCondition.text = snapshot.value as? String
+            
+        })
+        */
     }
     
+    /*
+    @IBAction func updateWeatherButton(sender: UIButton) {
+        ref.setValue(sender.titleLabel?.text)
+        
+    }
+    */
     
+    @IBAction func UpdateUserProfile(sender: AnyObject) {
+        
+        var usersRef = ref.childByAppendingPath("users")
+        
+        var users = ["kyleisawesome": kyleisawesome]
+
+        usersRef.setValue(users)
+        
+        usersRef.childByAppendingPath("kyleisawesome").setValue(kyleisawesome)
+        
+        var kyleRef = usersRef.childByAppendingPath("kyleisawesome")
+        var nickname = ["nickname": "Amazing Grace"]
+        
+        kyleRef.updateChildValues(nickname)
+        
+        
+        usersRef.updateChildValues([
+            "kyleisawesome/nickname": "Amazing Grace version 2",
+            "kyleisawesome/FirstName": "Carson",
+            "kyleisawesome/AdditionalFact": "Hawaii"
+            ])
+        
+        /*
+        ref.setValue(<#T##value: AnyObject?##AnyObject?#>, forKey: <#T##String#>)
+        ref.setValue(<#T##value: AnyObject!##AnyObject!#>)
+        
+        ref.childByAppendingPath("users/cpgarrett10@gmail.com/name")
+        */
+    }
 }

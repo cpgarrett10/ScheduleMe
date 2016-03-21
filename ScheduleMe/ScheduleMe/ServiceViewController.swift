1dd2534e38f88b98d68e792c23f1dc59524e5d41
danmo91 <dan.morain91@gmail.com>
2016-03-21 11:17:10 -0600

    fixed pods

--- ScheduleMe/ScheduleMe/ServiceViewController.swift
+++ ScheduleMe/ScheduleMe/ServiceViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ServiceViewController : UIViewController {
     
--- ScheduleMe/ScheduleMe/UserProfileViewController.swift
+++ ScheduleMe/ScheduleMe/UserProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UserProfileViewController : UIViewController {
     
